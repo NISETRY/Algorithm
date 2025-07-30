60e63f2cbcb078d2a6fc5ae9b1f4cb86c3579522
최영권 <tigerkok@naver.com>
2025-07-31 00:46:46 +0900

    백준 단계별 풀이

--- 최영권/bk_9291.py
+++ 최영권/bk_9291.py
@@ -1,2 +1,24 @@
 T =  int(input())
-sudoku = []
+for tc in range(1, T+1):
+    sudoku = [list(map(int, input().split())) for _ in range(9)]
+    
+    for r in range(9):
+        row = set()
+        col = set()
+        for c in range(9):
+            row.add(sudoku[r][c])
+            col.add(sudoku[c][r])  
+            if (r % 3 == 0) and (c % 3 == 0):
+                square = set() 
+                for x in range(3):
+                    for y in range(3):
+                    # print(r, c, x, y)
+                        square.add(sudoku[r+x][c+y])
+        if len(row) != 9 or len(col) != 9 or len(square) != 9:
+            print(f"Case {tc}: INCORRECT")     
+            break   
+    else:
+        print(f"Case {tc}: CORRECT")
+
+    if tc != T:
+        input()
