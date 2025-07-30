0b49642a5796a551bf8fa35bfcc24e62902dee7c
hsjangjang <99gktjs2937@naver.com>
2025-07-31 00:33:32 +0900

    4949 풀이 완료

--- 장하선/baekjoon/4949.py
+++ 장하선/baekjoon/4949.py
@@ -1,2 +1,24 @@
-def eq():
-    
+while True:
+    str=input()
+    stack=[]
+    if str=='.':
+        break
+    for x in str:
+        if x=='(' or x=='[':
+            stack.append(x)
+        elif x==')':    
+            if len(stack)>0 and stack[-1]=='(':
+                stack.pop()
+            else:
+                stack.append(x)
+                break
+        elif x==']':
+            if len(stack)>0 and stack[-1]=='[':
+                stack.pop()
+            else:
+                stack.append(x)
+                break
+    if len(stack)==0:
+        print('yes')
+    else:
+        print('no')
