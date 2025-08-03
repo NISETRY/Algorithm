670ad19be79a96898d0de8fc808e1c1dfac0ea77
hsjangjang <99gktjs2937@naver.com>
2025-08-03 23:01:16 +0900

    2178 미로찾기 풀이 업로드

--- 장하선/week1/boj_2178_미로찾기.py
+++ 장하선/week1/boj_2178_미로찾기.py
@@ -1,7 +1,6 @@
 from collections import deque
 n,m=map(int,input().split())
 maze=[[] for _ in range(n)]
-# 방문 표시 : maze와 같은 크기의 boolean 행렬인 visited를 만들어줌
 visited = [[False]*m for _ in range(n)]
 visited[0][0]=True
 queue=deque([(0,0)])
@@ -9,8 +8,6 @@ for i in range(n):
     k=input()
     for j in range(m):
         maze[i].append(int(k[j]))
-# 현 위치 x,y에 대해 이동 가능한 경우는 x+-1, y+-1 4개
-# 막다른 길인 경우, pop으로 해당 리스트 반환 후 백트래킹
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 sc=0
@@ -23,5 +20,4 @@ while queue:
                     queue.append((nx,ny))
                     visited[nx][ny]=True
                     maze[nx][ny]=maze[x][y]+1
-                    print(*queue)
 print(maze[n-1][m-1])
