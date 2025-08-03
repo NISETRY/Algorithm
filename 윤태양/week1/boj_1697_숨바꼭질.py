36e0b5e8aaab5729975408158db8ff05fa9c00b0
youn-sun <sunxo1215@gmail.com>
2025-08-03 21:53:11 +0900

    숨바꼭질 거꾸로 버젼

--- 윤태양/week1/boj_1697_숨바꼭질.py
+++ 윤태양/week1/boj_1697_숨바꼭질.py
@@ -1,11 +1,14 @@
-n, k = map(int, input().split())
+k, n = map(int, input().split())
 visited = []
 
 def act(n):
-    move1 = 2*n
+    move1 = int(n/2)
     move2 = n+1
     move3 = n-1
-    return [move1, move2, move3]
+    if n%2==0:
+        return [move1,move2,move3]
+    else:
+        return [move2,move3]
 
 def bfs(n):
     visited.append(n)
@@ -26,7 +29,6 @@ def bfs(n):
                     temp.append(a[j])
                 temp.append(i)
                 que.append(temp)
-                print(temp)
                 
 print(len(bfs(n))-1)
                                 
