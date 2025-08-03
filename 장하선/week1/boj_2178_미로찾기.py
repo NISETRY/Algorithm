ff7f01d210bb1be49c11898f0f27f379b9dd2d32
hsjangjang <99gktjs2937@naver.com>
2025-08-03 22:41:31 +0900

    2178 미로찾기 풀이 업로드

--- 장하선/week1/boj_2178_미로찾기.py
+++ 장하선/week1/boj_2178_미로찾기.py
@@ -1,29 +1,26 @@
+from collections import deque
 n,m=map(int,input().split())
 maze=[[] for _ in range(n)]
-visited=[[0,0]]
-queue=[[0,0]]
+# 방문 표시 : maze와 같은 크기의 boolean 행렬인 visited를 만들어줌
+visited = [[False]*m for _ in range(n)]
+visited[0][0]=True
+queue=deque([(0,0)])
 for i in range(n):
     k=input()
     for j in range(m):
         maze[i].append(int(k[j]))
 # 현 위치 x,y에 대해 이동 가능한 경우는 x+-1, y+-1 4개
-# 이미 방문한 인덱스는 빈 리스트에 저장
 # 막다른 길인 경우, pop으로 해당 리스트 반환 후 백트래킹
-# 현재 문제 : 4방향을 모두 순회하다 보니 안 막히는 길이여도 한번 방문하면 돌아가버림
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
+sc=0
 while queue:
-    x,y=map(int, queue[-1])
+    x,y=queue.popleft()
     for i in range(4):
         nx,ny=x+dx[i],y+dy[i]
         if 0<=nx<n and 0<=ny<m:
-            if maze[nx][ny]==1:
-                if [nx,ny] not in visited:
-                    queue.append([nx,ny])
-                    visited.append([nx,ny])
-                    x,y=nx,ny
-                else:
-                    queue.pop()
-                    x=queue[-1][0]
-                    y=queue[-1][1]
-print(len(queue))
+            if maze[nx][ny]!=0 and not visited[nx][ny]:
+                    queue.append((nx,ny))
+                    visited[nx][ny]=True
+                    maze[nx][ny]=maze[x][y]+1
+print(maze[n-1][m-1])
