9145762a4468255622450c50eb9deb21b4d15106
hsjangjang <99gktjs2937@naver.com>
2025-08-03 23:56:20 +0900

    2667 단지번호붙이기 중간저장

---
+++ 장하선/week1/boj_2667_단지번호붙이기.py
@@ -0,0 +1,20 @@
+from collections import deque
+n=int(input())
+apt=[[0 for _ in range(n)] for _ in range(n)]
+apt_nums=[] # 최종 출력
+queue=deque([(0,0)])
+for i in range(n):
+    k=input()
+    for j in range(n):
+        apt[i][j]=int(k[j])
+# BFS로 접근, 1 나오면 상하좌우 검색해서 1인지 확인하고 1인거 안나올때까지 탐색
+# 방문한거는 visited로 처리
+# 주변에 0으로 둘러쌓였다->apt_nums에 붙은거 수 붙이기
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+while queue:
+    x,y=queue.popleft()
+    for i in range(4):
+        nx,ny=x+dx[i],y+dy[i]
+        if 0<=nx<n and 0<=ny<n:
+            
