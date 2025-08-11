355f5bb1cdaa39e143f52dc6fdef1915b96e8b0f
hsjangjang <99gktjs2937@naver.com>
2025-08-12 00:23:34 +0900

    안전영역, 단지번호 풀이 업로드

--- 장하선/week1/boj_2667_단지번호붙이기.py
+++ 장하선/week1/boj_2667_단지번호붙이기.py
@@ -3,8 +3,7 @@ n=int(input())
 apt=[[0 for _ in range(n)] for _ in range(n)]
 apt_nums=[] # 최종 출력
 visited=[[False]*n for _ in range(n)]
-visited[0][0]=True
-queue=deque([(0,0)])
+# 입력 처리
 for i in range(n):
     k=input()
     for j in range(n):
@@ -14,13 +13,27 @@ for i in range(n):
 # 주변에 0으로 둘러쌓였다->apt_nums에 붙은거 수 붙이기
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
-while queue:
-    x,y=queue.popleft()
-    apt_nums.extend([0])
-    for i in range(4):
-        nx,ny=x+dx[i],y+dy[i]
-        if 0<=nx<n and 0<=ny<n:
-            if not visited[nx][ny] and apt[nx][ny]==1:
-                queue.append((nx,ny))
-                apt_nums[-1]+=1
-print(apt_nums)
+# 아파트 있는 부분만 반복문 돌고 단지에 포함시키기
+for i in range(n):
+    for j in range(n):
+        if apt[i][j]==1 and not visited[i][j]:
+            queue=deque([(i,j)])
+            visited[i][j]=True
+            cnt=1
+            while queue:
+                x,y=queue.popleft()
+                for d in range(4):
+                    nx,ny=x+dx[d],y+dy[d]
+                    if 0<=nx<n and 0<=ny<n:
+                        if apt[nx][ny]==1 and not visited[nx][ny]:
+                            queue.append((nx,ny))
+                            visited[nx][ny]=True
+                            cnt+=1
+            apt_nums.append(cnt)
+
+# 정답 출력 처리            
+apt_nums.sort()
+d=len(apt_nums)
+print(d)
+for i in range(d):
+    print(apt_nums[i])
