495437267eb50037251a379fc8f165fc45f8412c
saehyunkim <kimsaehyun01@naver.com>
2025-08-13 17:42:36 +0900

    수업

--- 김세현/swea/swea_20397_돌 뒤집기 게임 2.py
+++ 김세현/swea/swea_20397_돌 뒤집기 게임 2.py
@@ -1,3 +1,24 @@
-N, M = map(int, input().split())
+T = int(input())
 
-P = input().split()
+for tc in range(T):
+
+    N, M = map(int, input().split())
+
+    stat = list(map(int, input().split()))
+
+    for _ in range(M):
+        i, j = map(int, input().split())
+
+        for d in range(1, j+1):
+            right = i+d-1
+            left = i-d-1
+            
+            if left < 0 or right >= N:
+                break
+
+            if stat[left] == stat[right]:
+                # 0 <-> 1 을 바꿔야 함
+                stat[right] = 1 - stat[right]
+                stat[left] = 1 - stat[left]
+            
+    print(f'#{tc+1}', *stat)
