8b5b387bec7c4b190ff224540db0bd7859d8daae
hsjangjang <gu1234890@gmail.com>
2025-08-19 17:56:49 +0900

    A형 문제 대충 정리, N-queen 중간저장

--- 장하선/week2/boj_17471_게리맨더링.py
+++ 장하선/week2/boj_17471_게리맨더링.py
@@ -1,3 +1,7 @@
+# 선거구를 나눌 수 있는 모든 경우의 수 확인
+# 위에서 구한 case 별 인구차를 list에 저장
+# 
+from collections import deque
 n=int(input())
 population=list(map(int,input().split()))
 nearby=[[0 for _ in range(n+1)] for _ in range(n+1)]
@@ -5,5 +9,5 @@ for i in range(n):
     near=list(map(int,input().split()))
     for j in range(1,len(near)):
         nearby[i+1][near[j]]=nearby[near[j]][i+1]=1
-for i in range(len(nearby)):
-    print(nearby[i])
+queue=deque([1])
+def dfs():
