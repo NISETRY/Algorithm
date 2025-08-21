f51f49706e37cda9b22c83e5274832b1731d39f4
saehyunkim <kimsaehyun01@naver.com>
2025-08-21 17:40:26 +0900

    swea, BOJ

--- 김세현/swea/swea_2806_N-Queen.py
+++ 김세현/swea/swea_2806_N-Queen.py
@@ -1,5 +1,4 @@
 # visited 열 번호로 세팅 v[열] = 1 // 열 검사
-
 T = int(input())
 
 def n_queen(row):
@@ -11,7 +10,7 @@ def n_queen(row):
     
     for col in range(N):
         # 세로 검사 통과 & 좌하향 통과 & 우하향 통과
-        if not col_visited[col] and not main_diag_visited[row-col+N-1] and not sub_diag_visited[row+col]:
+        if col_visited[col] == 0 and main_diag_visited[row-col+N-1] == 0 and sub_diag_visited[row+col] == 0:
             col_visited[col] = 1
             main_diag_visited[row-col+N-1] = 1
             sub_diag_visited[row+col] = 1
@@ -21,7 +20,6 @@ def n_queen(row):
             col_visited[col] = 0
             main_diag_visited[row-col+N-1] = 0
             sub_diag_visited[row+col] = 0
-
 
 for tc in range(1, T+1):
     N = int(input())
