7938e2180d6a8374d3c9eb8c0074050a8a4ea098
Tyler <>
2025-09-25 10:10:07 +0900

    감소하는 수

--- 장하선/week5/boj_1038_감소하는 수.py
+++ 장하선/week5/boj_1038_감소하는 수.py
@@ -1,1 +1,10 @@
 n=int(input())
+nums=[]
+for mask in range(1,1<<10):
+    sub=[]
+    for i in range(9,-1,-1):
+        if mask&(1<<i):
+            sub.append(str(i))
+    nums.append(int(''.join(sub)))
+nums.sort()
+print(nums[n] if n<1023 else -1)
