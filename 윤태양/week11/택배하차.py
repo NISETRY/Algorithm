df03032850ba36dea15b14dc5d25f2e085ba38de
ODFG <jjun1070@naver.com>
2025-11-12 17:49:16 +0900

    fuck

--- 윤태양/week11/택배하차.py
+++ 윤태양/week11/택배하차.py
@@ -1,17 +1,35 @@
+from pprint import pprint
+
+def drop(k,h,w,c,pos):
+    global graph
+    
+    can_drop = True
+    
+    while can_drop:
+        for width in range(w):
+            if pos+h<n and graph[pos+h][c+width] == 0:
+                pass 
+            else:
+                can_drop = False
+                
+        if can_drop:
+            pos += 1
+    
+    for i in range(h):
+        for j in range(w):
+            graph[pos+i][j+c] = k
+            
+def left_off():
+    left = [0 for _ in range(1,m+1)]
+    for 
+    
+
+
 n, m = map(int, input().split()) # 그래프 크기, 택배 개수
 graph = [[0]*n for _ in range(n)]
 for _ in range(m):
-    k, h, w, c = map(int, input().split()) # 번호, 세로크기, 가로길이, c 왼쪽 위 위치
-                                           # c, c+w까지 싹다 0이면 아래로 하강 
-                                           # 택배는 싹다 택배번호로 채워놓고
-                                           # 높이는 어차피 0에서 시작
-                                           # 아래로 하강할 때마다 높이 +1해서 n까지 or n-h까지
+    k, h, w, c = map(int, input().split()) # 번호, 세로 크기, 가로 크기, 좌측 좌표
+    c -= 1
+    drop(k,h,w,c,0)
     
-    
-    
-    
-
-# 다 빼고 난뒤
-# 오른쪽 빼고 하강 
-# 왼쪽 빼고 하강
-# 순서 기록 -> 끝
+    pprint(graph)
